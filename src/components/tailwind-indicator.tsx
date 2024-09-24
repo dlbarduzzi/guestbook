48dcfc068d7aef1529e5e10c4f39ec509d3f8286
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-09-24 09:10:06 -0400

    Fix lint and format

--- src/components/tailwind-indicator.tsx
+++ src/components/tailwind-indicator.tsx
@@ -1,7 +1,9 @@
 import { cn } from "@/lib/utils"
 
+import { env } from "@/env/server"
+
 export function TailwindIndicator() {
-  if (process.env.NODE_ENV === "production") {
+  if (env.NODE_ENV === "production") {
     return null
   }
   return (
@@ -12,7 +14,9 @@ export function TailwindIndicator() {
       )}
     >
       <div className="block sm:hidden">xs</div>
-      <div className="hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden">sm</div>
+      <div className="hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden">
+        sm
+      </div>
       <div className="hidden md:block lg:hidden xl:hidden 2xl:hidden">md</div>
       <div className="hidden lg:block xl:hidden 2xl:hidden">lg</div>
       <div className="hidden xl:block 2xl:hidden">xl</div>
