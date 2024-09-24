7ee5033b4a8522ec96999bfa29263d1a2bf24745
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-09-23 23:49:10 -0400

    Update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,19 @@
+import { siteConfig } from "@/lib/site"
+
 import { Container } from "./container"
 
 export function Footer() {
   return (
     <footer className="border-t border-t-gray-200 bg-white">
-      <Container>
-        <div className="flex items-center py-4">Footer</div>
+      <Container className="py-5 md:flex md:items-center md:justify-between">
+        <div>
+          <p className="text-center text-sm text-gray-500">
+            &copy; {new Date().getFullYear()} {siteConfig.name}
+          </p>
+        </div>
+        <div className="pt-1 text-center text-sm text-gray-500 md:pt-0">
+          Created by <span className="font-semibold">Dylan Barduzzi</span>
+        </div>
       </Container>
     </footer>
   )
